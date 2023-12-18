a49f0b0701a51dcda45b4c172e06a1832bd8e95b
VGiudicelli <vincent.giudicelli@ensg.eu>
2023-12-18 16:39:29 +0100

    add class PathNode and migrate attributes

--- src/main/java/org/acme/graph/model/Vertex.java
+++ src/main/java/org/acme/graph/model/Vertex.java
@@ -1,11 +1,11 @@
 package org.acme.graph.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.locationtech.jts.geom.Coordinate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
  * 
@@ -25,19 +25,6 @@ public class Vertex {
 	 * Position du sommet
 	 */
 	private Coordinate coordinate;
-
-	/**
-	 * dijkstra - coût pour atteindre le sommet
-	 */
-	private double cost;
-	/**
-	 * dijkstra - arc entrant avec le meilleur coût
-	 */
-	private Edge reachingEdge;
-	/**
-	 * dijkstra - indique si le sommet est visité
-	 */
-	private boolean visited;
 
 	private List<Edge> inEdges;
 	private List<Edge> outEdges;
@@ -79,32 +66,6 @@ public class Vertex {
 
 	public void setCoordinate(Coordinate coordinate) {
 		this.coordinate = coordinate;
-	}
-
-	@JsonIgnore
-	public double getCost() {
-		return cost;
-	}
-
-	public void setCost(double cost) {
-		this.cost = cost;
-	}
-
-	@JsonIgnore
-	public Edge getReachingEdge() {
-		return reachingEdge;
-	}
-
-	public void setReachingEdge(Edge reachingEdge) {
-		this.reachingEdge = reachingEdge;
-	}
-
-	public boolean isVisited() {
-		return visited;
-	}
-
-	public void setVisited(boolean visited) {
-		this.visited = visited;
 	}
 
 	@Override
